d57daa8d0110242047744c12b593fc65ac80da60
amog1 <160067063+amog1@users.noreply.github.com>
2024-03-15 13:49:40 -0600

    Add files via upload

---
+++ Hello-World-Event-Listeners/main.js
@@ -0,0 +1,20 @@
+// JAVASCRIPT EVENT LISTENERS
+
+// var heading = document.getElementById("test");
+// console.log(heading);
+
+// var listItem = document.getElementById("list2");
+// console.log(listItem);
+
+// Greet
+
+// // declare a function
+// function greet() {
+//   let name = prompt(" Enter your name");
+//   alert(`Nice to meet you, ${name}`);
+// }
+// // greet();
+// // greet();
+// // greet();
+
+// document.getElementById("b1").addEventListener("click", greet);
